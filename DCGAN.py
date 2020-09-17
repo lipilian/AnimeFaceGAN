4ddfb588a06285979806fd953cfa3df9aeb70c45
Hong <liuhong2@illinois.edu>
2020-09-17 16:39:58 -0500

    train

--- DCGAN.py
+++ DCGAN.py
@@ -1,5 +1,7 @@
 #%% import package
 import numpy as np
+import ipdb
+import glob
 import torch as t 
 import torchvision as tv 
 import os 
@@ -8,7 +10,12 @@ import matplotlib.pyplot as plt
 import time
 import random
 import visdom
+from visdom import server
+from torchnet.meter import AverageValueMeter
+# in order to use visdom via ssh, set ssh config forawrd
+# 127.0.0.1:8097 to local 127.0.0.1:8097
 from torch import nn
+from visualize import Visualizer
 # %%set the random seed for reproduction
 manualSeed = 980
 print("Random Seed: ", manualSeed)
@@ -23,6 +30,7 @@ class Config(object):
     Workers = 0 # the number of processor used for data loading 
     ImgSize = 96
     BatchSize = 256
+    MaxEpochs = 400
     NumChannels = 3 # color image has R G B channels
     LRG = 2e-4 # learning rate of generator
     LRD = 2e-4 # learning rate of discriminator
@@ -44,6 +52,7 @@ class Config(object):
     SaveEvery = 10 # save model every N batch
     NetDPath = None # ./checkpoints/NetD_.pth pretrained discriminator model
     NetGPath = None # ./checkpoints/NetG_.pth pretrained generator model
+    checkpointsPath ='./checkpoints/'
 
     # test without training 
     GenImg = './result.png'
@@ -141,44 +150,93 @@ class Discriminator(nn.Module):
     def forward(self, input):
         return self.main(input).view(-1)
 # %% start training
-def train():
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+opt.Vis = True # set the visulization to true by using visdom
+# run python -m visdom.server in terminal
+if opt.Vis:
+    vis = Visualizer(opt.env)
+# create generator and discriminator
+netG, netD = Generator(opt), Discriminator(opt)
+# load pretrained check points
+map_location = lambda storage, loc: storage
+netDCheckPath = 
+if opt.NetDPath:
+    netD.load_state_dict(t.load(opt.NetDPath, map_location=map_location))
+if opt.NetGPath:
+    netG.load_state_dict(t.load(opt.NetGPath, map_location=map_location))
+netG.to(device)
+netD.to(device)
+netG.apply(weights_init);
+netD.apply(weights_init);
+# %% define lost and optimizer
+criterion = t.nn.BCELoss().to(device)
+optimizerG = t.optim.Adam(netG.parameters(), lr = opt.LRG, betas=(opt.Beta, 0.999))
+optimizerD = t.optim.Adam(netD.parameters(), lr = opt.LRD, betas=(opt.Beta, 0.999))
+# define true label and fake label 
+TrueLabel = t.ones(opt.BatchSize).to(device)
+FakeLabel = t.zeros(opt.BatchSize).to(device)
+fix_noises = t.randn(opt.BatchSize, opt.Nz, 1, 1).to(device)
+errorGMeter = AverageValueMeter()
+errorDMeter = AverageValueMeter()
+# %% start training
+print('start training ......')
+for epoch in range(opt.MaxEpochs):
+    for i, (img,_) in tqdm.tqdm(enumerate(dataloader)):
+        realImg = img.to(device) # transfer 256,3,96,96 image to CUDA
+        ############################
+        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        ###########################
+        if i % opt.DEvery == 0: # train discriminator
+            #maximize log(D(x)) + log(1 - D(G(z)))
+            optimizerD.zero_grad()
+            # train all real image with true label
+            output = netD(realImg).view(-1)
+            errorDReal = criterion(output, TrueLabel)
+            errorDReal.backward()
+            # train all fake image with fake label
+            noises = t.randn(opt.BatchSize, opt.Nz, 1, 1, device = device)
+            fakeImg = netG(noises) 
+            output = netD(fakeImg.detach()).view(-1)# turn off gradient tracking to avoid generator training
+            errorDFake = criterion(output, FakeLabel)
+            errorDFake.backward()
+            # update all weight
+            optimizerD.step()
+            # collect all errors
+            errorD = errorDReal + errorDFake
+            errorDMeter.add(errorD.item())
+        ############################
+        # (2) Update G network: maximize log(D(G(z)))
+        ###########################
+        if i % opt.GEvery == 0: # train generator 
+            optimizerG.zero_grad()
+            if i % opt.DEvery != 0: # if discriminator is not trained within this batch
+                noises = t.randn(opt.BatchSize, opt.Nz, 1, 1, device = device)
+                fakeImg = netG(noises)
+            # if discriminator got trained, we already have trained fakeImg
+            output = netD(fakeImg).view(-1)
+            errorG = criterion(output, TrueLabel)
+            errorG.backward()
+            optimizerG.step()
+            errorGMeter.add(errorG.item())
+        ############################
+        # (3) visualize with visdom
+        ###########################  
+        if opt.Vis and i % opt.plot_every == opt.plot_every - 1:
+            if os.path.exists(opt.DebugFile):
+                ipdb.set_trace()
+            fixFakeImg = netG(fix_noises)
+            vis.images(fixFakeImg.detach().cpu().numpy()[:64] * 0.5 + 0.5, win = 'fixFakeImg')
+            vis.images(realImg.data.cpu().numpy()[:64] * 0.5 + 0.5, win = 'realImg')
+            vis.plot('ErrorGenerator', errorGMeter.value()[0])
+            vis.plot('ErrorDiscriminator', errorDMeter.value()[0])
+    ############################
+    # (3) save training result
+    ###########################        
+    if (epoch + 1) % opt.SaveEvery == 0: 
+        tv.utils.save_image(fixFakeImg.data[:64], '%s/%05d.png' % (opt.SavePath, epoch), normalize=True, range = (-1,1))
+        t.save(netD.state_dict(), './checkpoints/netD_%05d.pth' % epoch) 
+        t.save(netG.state_dict(), './checkpoints/netG_%05d.pth' % epoch)      
+        errorDMeter.reset()
+        errorGMeter.reset()
 # %%
-'''
-if __name__ == '__main__':
-    startTime = time.time()
-    test_batch = next(iter(dataloader))
-    print("The time for dataloader with %d of workers: %f" % (opt.Workers, time.time() - startTime))
-    plt.figure(figsize=(8,8))
-    plt.axis('off')
-    plt.title('training imgs')
-    plt.imshow(np.transpose(tv.utils.make_grid(test_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
-    plt.savefig('1.jpg')
-'''
+t.save(netD.state_dict(), './checkpoints/netD_%05d.pth' % 9) 
 # %%
